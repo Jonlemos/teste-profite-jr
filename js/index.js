6311fe63523f516a7b697d8e3d6cdbf8e91bb925
Jonlemos <lemos.anjos@gmail.com>
2019-11-22 18:12:23 -0300

    init commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+// $(document).ready(() => {
+    $('#basicSlider').multislider({
+        continuous: true,
+        duration: 2000
+    });
+    $('#mixedSlider').multislider({
+        duration: 750,
+        interval: 3000
+    });
+
+
+// })
+
